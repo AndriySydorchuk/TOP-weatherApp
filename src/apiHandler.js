3aa749e3991397799d1f0f1c810ff1774e61d2ae
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-30 15:08:18 +0300

    Add api handler

---
+++ src/apiHandler.js
@@ -0,0 +1,26 @@
+const apiHandler = (() => {
+  const API_KEY = "REDACTED";
+  const API_ENDPOINT =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+  async function getWeatherData(location) {
+    try {
+      const response = await fetch(`${API_ENDPOINT}${location}?key=${API_KEY}`);
+
+      if (!response.ok) {
+        throw new Error(
+          `HTTP Error: ${response.status} ${response.statusText}`,
+        );
+      }
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Fetch failed: ", error);
+    }
+  }
+
+  return { getWeatherData };
+})();
+
+export { apiHandler };
