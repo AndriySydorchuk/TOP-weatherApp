5e134bef08f2c0d187f67e604fa3bc2b0e0e48ee
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-30 17:43:15 +0300

    Remove unnecessary imports and function calls

--- src/index.js
+++ src/index.js
@@ -1,29 +1,4 @@
 import "./style.css";
-import { apiHandler } from "./apiHandler";
-import { convertFahrenheitToCelsius, convertMphToMs } from "./convert";
+import { eventManager } from "./eventManager";
 
-const userLocation = prompt("Type city name");
-
-apiHandler.getWeatherData(userLocation).then(function (weatherData) {
-  console.log(
-    `${convertFahrenheitToCelsius(weatherData.currentConditions.temp)}°C`,
-  );
-
-  console.log(weatherData.description);
-
-  console.log(weatherData.resolvedAddress);
-
-  console.log(
-    `Feels like: ${convertFahrenheitToCelsius(weatherData.currentConditions.feelslike)}°C`,
-  );
-
-  console.log(
-    `Humidity: ${Math.round(weatherData.currentConditions.humidity)}%`,
-  );
-
-  console.log(
-    `Wind speed: ${convertMphToMs(weatherData.currentConditions.windspeed)} m/s`,
-  );
-
-  console.log(`Day: ${weatherData.days[0].datetime}`);
-});
+eventManager.init();
