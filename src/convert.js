dff4f1856a53b3888e8926d57d577afd3d042684
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-30 17:39:57 +0300

    Add function to format weather data

--- src/convert.js
+++ src/convert.js
@@ -1,7 +1,21 @@
-export function convertFahrenheitToCelsius(temperatureInFahrenheit) {
+function convertFahrenheitToCelsius(temperatureInFahrenheit) {
   return Math.trunc((temperatureInFahrenheit - 32) * (5 / 9));
 }
 
-export function convertMphToMs(mph) {
+function convertMphToMs(mph) {
   return (mph * 0.44704).toFixed(2);
+}
+
+export function formatWeatherData(weatherData) {
+  return {
+    temperature: convertFahrenheitToCelsius(weatherData.currentConditions.temp),
+    description: weatherData.description,
+    location: weatherData.resolvedAddress,
+    feelslike: convertFahrenheitToCelsius(
+      weatherData.currentConditions.feelslike,
+    ),
+    humidity: Math.round(weatherData.currentConditions.humidity),
+    windspeed: convertMphToMs(weatherData.currentConditions.windspeed),
+    day: weatherData.days[0].datetime,
+  };
 }
