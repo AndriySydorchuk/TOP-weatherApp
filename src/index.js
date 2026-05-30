ebf82299b91ad74cec502008af1ca626dac43d61
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-30 15:19:10 +0300

    Add basic app logic, user locaion input, api request and weather info display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import "./style.css";
+import { apiHandler } from "./apiHandler";
+import { convertFahrenheitToCelsius, convertMphToMs } from "./convert";
 
-console.log("work");
+const userLocation = prompt("Type city name");
+
+apiHandler.getWeatherData(userLocation).then(function (weatherData) {
+  console.log(
+    `${convertFahrenheitToCelsius(weatherData.currentConditions.temp)}°C`,
+  );
+
+  console.log(weatherData.description);
+
+  console.log(weatherData.resolvedAddress);
+
+  console.log(
+    `Feels like: ${convertFahrenheitToCelsius(weatherData.currentConditions.feelslike)}°C`,
+  );
+
+  console.log(
+    `Humidity: ${Math.round(weatherData.currentConditions.humidity)}%`,
+  );
+
+  console.log(
+    `Wind speed: ${convertMphToMs(weatherData.currentConditions.windspeed)} m/s`,
+  );
+
+  console.log(`Day: ${weatherData.days[0].datetime}`);
+});
