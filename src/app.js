d4c620509586d0779f5838758b7d8fb6815ce48b
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-30 23:48:26 +0300

    Add function to translate to english using api

--- src/app.js
+++ src/app.js
@@ -13,3 +13,20 @@ export function loadWeather(userLocation) {
     console.log(data);
   });
 }
+
+export async function translateUkToEn(text) {
+  try {
+    const response = await fetch(
+      `https://api.mymemory.translated.net/get?q=${encodeURIComponent(text)}&langpair=uk|en`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}, ${response.statusText}`);
+    }
+
+    const translateData = await response.json();
+    return translateData;
+  } catch (error) {
+    console.error("Fetch error", error);
+  }
+}
