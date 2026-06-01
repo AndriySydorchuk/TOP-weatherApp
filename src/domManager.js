4b206af8a47992e1ad7706aef2484b52a9707a39
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 11:32:05 +0300

    Remove location translating

--- src/domManager.js
+++ src/domManager.js
@@ -1,5 +1,3 @@
-import { translateUkToEn } from "./app";
-
 const domManager = (() => {
   const userInputEl = document.getElementById("cityInput");
 
@@ -21,10 +19,7 @@ const domManager = (() => {
   function setData(weatherData) {
     temperatureEl.textContent = weatherData.temperature + "°C";
     descriptionEl.textContent = weatherData.description;
-
-    translateUkToEn(weatherData.location).then(function (translateData) {
-      locationEl.textContent = translateData.responseData.translatedText;
-    });
+    locationEl.textContent = weatherData.location;
 
     feelslikeEl.textContent = weatherData.feelslike + "°C";
     humidityEl.textContent = weatherData.humidity + "%";
