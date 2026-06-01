d712cd79e0091f17ee5a427309194e3ea33a79fb
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 13:10:32 +0300

    Remove converting functions

--- src/weatherFormatter.js
+++ src/weatherFormatter.js
@@ -1,17 +1,13 @@
-import { convertFahrenheitToCelsius, convertMphToMs } from "./convertUnits";
 import { formatDate } from "./dateFormatter";
 
 export function formatWeatherData(weatherData) {
   return {
-    temperature:
-      convertFahrenheitToCelsius(weatherData.currentConditions.temp) + "°C",
+    temperature: Math.round(weatherData.currentConditions.temp) + "°C",
     description: weatherData.description,
     location: weatherData.resolvedAddress,
-    feelslike:
-      convertFahrenheitToCelsius(weatherData.currentConditions.feelslike) +
-      "°C",
+    feelslike: Math.round(weatherData.currentConditions.feelslike) + "°C",
     humidity: Math.round(weatherData.currentConditions.humidity) + "%",
-    windspeed: convertMphToMs(weatherData.currentConditions.windspeed) + "m/s",
+    windspeed: weatherData.currentConditions.windspeed + " km/h",
     day: formatDate(weatherData.days[0].datetime),
   };
 }
