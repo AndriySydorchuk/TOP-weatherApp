04ac2430f851b2d50d2ba1268194c40c3003f30c
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 11:51:32 +0300

    Remove convertaion functions from index.js

--- src/index.js
+++ src/index.js
@@ -23,14 +23,6 @@ gobackBtn.addEventListener("click", () => {
   domManager.renderHomeView();
 });
 
-export function convertFahrenheitToCelsius(temperatureInFahrenheit) {
-  return Math.trunc((temperatureInFahrenheit - 32) * (5 / 9));
-}
-
-export function convertMphToMs(mph) {
-  return (mph * 0.44704).toFixed(2);
-}
-
 export function formatDate(dateStr) {
   const date = parseISO(dateStr);
   return format(date, "EEEE, MMMM d, yyyy");
