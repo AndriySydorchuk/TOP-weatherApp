ea3cd424e2f15fcab83c0cb155ed031b44bdb3c8
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 12:47:09 +0300

    Add app module to coordinate logic inside it, not in index.js

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import { apiService } from "./apiService";
+import { domManager } from "./domManager";
+import { formatWeatherData } from "./weatherFormatter";
+
+const app = (() => {
+  const userInputEl = document.getElementById("cityInput");
+  const gobackBtn = document.getElementById("gobackBtn");
+
+  function handleSearch() {
+    const userLocation = userInputEl.value.trim();
+
+    if (!userLocation) return;
+
+    apiService
+      .getWeatherData(userLocation)
+      .then((weatherData) => {
+        console.log(weatherData);
+        return formatWeatherData(weatherData);
+      })
+      .then(domManager.renderInfoView)
+      .catch((err) => domManager.showFailed(userLocation));
+  }
+
+  function init() {
+    userInputEl.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        handleSearch();
+      }
+    });
+
+    gobackBtn.addEventListener("click", () => {
+      domManager.renderHomeView();
+    });
+  }
+
+  return { init };
+})();
+
+export { app };
