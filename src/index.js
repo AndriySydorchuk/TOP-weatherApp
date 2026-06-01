120ca00c5880f30781f6b82c2f737baf7fd113dd
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 12:47:39 +0300

    Move app logic to app module and call app.init()

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import "./style.css";
 
-import { apiService } from "./apiService";
-import { domManager } from "./domManager";
-import { formatWeatherData } from "./weatherFormatter";
+import { app } from "./app";
 
-const userInputEl = document.getElementById("cityInput");
-const gobackBtn = document.getElementById("gobackBtn");
-
-userInputEl.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    const userLocation = userInputEl.value.trim();
-
-    apiService
-      .getWeatherData(userLocation)
-      .then((weatherData) => {
-        console.log(weatherData);
-        return formatWeatherData(weatherData);
-      })
-      .then(domManager.renderInfoView)
-      .catch((err) => domManager.showFailed(userLocation));
-  }
-});
-
-gobackBtn.addEventListener("click", () => {
-  domManager.renderHomeView();
-});
+app.init();
