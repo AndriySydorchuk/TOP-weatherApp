afab77f19870a82cda12401134dc0f7443eb7a1a
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 13:10:11 +0300

    Remove convertUnits module because requesting data from api already in metric system it doesn't make sense

--- src/convertUnits.js
+++
@@ -1,7 +0,0 @@
-export function convertFahrenheitToCelsius(temperatureInFahrenheit) {
-  return Math.trunc((temperatureInFahrenheit - 32) * (5 / 9));
-}
-
-export function convertMphToMs(mph) {
-  return (mph * 0.44704).toFixed(2);
-}
