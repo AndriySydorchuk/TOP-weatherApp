3e7d03899fc78694ebb6147df836fe437107c378
Andriy Sydorchuk <programmers130822@gmail.com>
2026-06-01 13:57:36 +0300

    Add iconsMap to correspond visual crossing api data with lucide icons

---
+++ src/iconsMap.js
@@ -0,0 +1,12 @@
+import { Sun, Moon, Cloud, CloudRain, CloudSun, CloudMoon } from "lucide";
+
+const iconsMap = {
+  "partly-cloudy-day": CloudSun,
+  "partly-cloudy-night": CloudMoon,
+  rain: CloudRain,
+  cloudy: Cloud,
+  "clear-day": Sun,
+  "clear-night": Moon,
+};
+
+export { iconsMap };
